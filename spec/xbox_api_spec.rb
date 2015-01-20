9dd2e12dd3b21cabc99ac7cebb66e0f07fa202bb
audibleblink <virtualsnyper@gmail.com>
2015-01-20 16:35:58 -0500

    add cassette for when a player is offline

--- spec/xbox_api_spec.rb
+++ spec/xbox_api_spec.rb
@@ -1,45 +1,64 @@
 require 'spec_helper'
 require 'vcr'
 
-describe "Gem" do
+describe XboxApi do
 
 let(:token)  {File.read('./.env').chomp.split(" ")[1]}
 let(:client) {XboxApi::Client.new(token)}
-let(:gamer)  do 
-  VCR.use_cassette("gamer") { client.gamer("audibleblink") }
-end
+let(:gamer)  {  VCR.use_cassette("gamer") { client.gamer("audibleblink") }  }
 
-describe XboxApi::Client do
-  
+  describe XboxApi::Client do
 
-  context "#gamer" do 
+    context "#gamer" do 
 
-    it "returns a Gamer object" do
-      expect( gamer ).to be_a_kind_of XboxApi::Gamer
+      it "returns a Gamer object" do
+        expect( gamer ).to be_a_kind_of XboxApi::Gamer
+      end
+
     end
 
-  end
-
-  context "#calls_remaining" do 
-    it "return a hash with the correct info" do
-      VCR.use_cassette("calls_remaining") do
-        response = client.calls_remaining
-        expect(response).to be_a_kind_of Hash
+    context "#calls_remaining" do 
+      it "returns a hash with the correct info" do
+        VCR.use_cassette("calls_remaining") do
+          response = client.calls_remaining
+          expect(response).to be_a_kind_of Hash
+          expect(response.keys.length).to eq 3
+        end
       end
     end
   end
-end
 
-describe XboxApi::Gamer do
+  describe XboxApi::Gamer do
 
-  context "#presence" do 
-    it "returns a players presence info" do
-      VCR.use_cassette("presence") do
-        response = gamer.presence
-        expect(response).to be_a_kind_of Hash
+    context "#presence" do 
+      
+      it "returns 'Offline' for an offline player" do
+        VCR.use_cassette("presence_offline") do
+          response = gamer.presence
+          expect(response[:state]).to eq "Offline"
+        end
+      end
+
+      xit "returns 'Online' for an online player" do
+        VCR.use_cassette("presence_online") do
+          response = gamer.presence
+          expect(response[:state]).to eq "Online"
+        end
+      end
+
+      xit "returns 'Away' for an away player" do
+        VCR.use_cassette("presence_away") do
+          response = gamer.presence
+          expect(response[:state]).to eq "Away"
+        end
+      end
+    end
+
+    context "#xuid" do  
+      it "has an xuid" do
+        expect(gamer.xuid).to_not be nil
       end
     end
   end
-end
 
 end
