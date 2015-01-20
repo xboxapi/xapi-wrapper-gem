cc762b5be54fb78a540d81d2a0020afc7729096f
audibleblink <virtualsnyper@gmail.com>
2015-01-19 23:55:12 -0500

    metaprogram endpoint methods
    - eval xuid on instantiation
    - make xuid accessor public

--- lib/xbox-api/gamer.rb
+++ lib/xbox-api/gamer.rb
@@ -5,37 +5,26 @@ module XboxApi
   class Gamer
 
     attr_reader :gamertag
+    attr_accessor :xuid
 
     def initialize(gamertag, client)
       @gamertag = gamertag
       @client   = client
-      @xuid     = nil
+      @xuid     = fetch_xuid
     end
 
-    def presence  
-      endpoint = "#{xuid}/#{__method__}"
-      get_with_token( endpoint )
-    end
+    [:presence, :profile, :gamercard, :activity, :friends, :followers]
+      .each do |action|
+        define_method( action ) do
+          endpoint = "#{xuid}/#{__method__}"
+          get_with_token( endpoint )
+        end
+      end
 
-    def profile
-      endpoint = "#{xuid}/#{__method__}"
-      get_with_token( endpoint )
-    end
-
-    def gamercard
-      endpoint = "#{xuid}/#{__method__}"
-      get_with_token( endpoint )
-    end
-
-    def activity
-      endpoint = "#{xuid}/#{__method__}"
-      get_with_token( endpoint )
-    end
 
     private
 
     attr_reader :client
-    attr_accessor :xuid
 
     def fetch_xuid
       endpoint = "xuid/#{gamertag}"
@@ -43,7 +32,6 @@ module XboxApi
     end
 
     def get_with_token(endpoint)
-      self.xuid ||= fetch_xuid
       request     = URI.parse("#{client.base_url}/#{endpoint}")
       response    = open(request, "X-AUTH" => client.api_key)
       response.read
