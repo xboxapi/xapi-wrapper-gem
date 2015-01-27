990db918ac55a983dd6e5454ecd4a29142528477
audibleblink <virtualsnyper@gmail.com>
2015-01-26 21:42:52 -0500

    add thor requirements

--- lib/xbox-api.rb
+++ lib/xbox-api.rb
@@ -1,5 +1,9 @@
 require "yajl"
 require "open-uri"
+require "thor"
+
 require "xbox-api/version"
 require "xbox-api/client"
 require "xbox-api/gamer"
+require_relative "./xbox-api/xboxlive"
+# require "xbox-api/xboxlive"
